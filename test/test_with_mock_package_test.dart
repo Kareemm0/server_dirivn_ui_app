d7e53c4fb16decd47d79740df7b3fa7f4e5df7c6
kareem_m0hmed <kareemmhammed53@gmail.com>
2026-04-26 22:46:27 +0300

    update: test data if in model or not

---
+++ test/test_with_mock_package_test.dart
@@ -0,0 +1,34 @@
+import 'package:flutter_test/flutter_test.dart';
+import 'package:http/http.dart' as http;
+import 'package:mockito/annotations.dart';
+import 'package:mockito/mockito.dart';
+import 'test_with_mock_package_test.mocks.dart' show MockClient;
+
+@GenerateMocks([http.Client])
+void main() {
+  group("uint Test On Api Call", () {
+    test("return data in success state or status ", () async {
+      MockClient client = MockClient();
+      when(
+        client.get(Uri.parse("https://jsonplaceholder.typicode.com/posts/1")),
+      ).thenAnswer((_) async {
+        return http.Response('{"title": "foo"}', 200);
+      });
+
+      /// Add Method which test on , and add return type or Model used to test
+      expect(await () {}, isA().having((e) => e.id, "id", 2));
+    });
+
+    test("Test in Failure State or Status ", () {
+      MockClient client = MockClient();
+      when(
+        client.get(Uri.parse("https://jsonplaceholder.typicode.com/posts/1")),
+      ).thenAnswer((_) async {
+        return http.Response('{"error": "Page not found"}', 404);
+      });
+
+      /// Add Method which test on
+      expect(() {}, throwsException);
+    });
+  });
+}
